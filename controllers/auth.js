7bba8b48b9e2ee398a757cbcfb0e4ab9f82ec5a5
ashish_kadam <ashish.kadam@quantiphi.com>
2023-07-23 02:12:37 +0530

    Defined a login functionality in controllers.Setup login route in routes auth. Defined getSignedToken & matchPassword methods in UserSchema.

--- controllers/auth.js
+++ controllers/auth.js
@@ -23,3 +23,36 @@ exports.register = async (req, res, next) => {
     next(err);
   }
 };
+
+// @desc     Login a user
+// @route    GET /api/v1/auth/login
+// @access   Public
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    // Validate email & password
+    if (!email || !password) {
+      return next(
+        new ErrorResponse("Please provide an email and password", 400)
+      );
+    }
+    // Check for user
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+      return next(
+        new ErrorResponse("User with the given email id does not exists", 401)
+      );
+    }
+    // Check if password matches
+    const isMatch = await user.matchPassword(password);
+    if (!isMatch) {
+      return next(new ErrorResponse("Incorrect Password", 401));
+    }
+    // Create token
+    const token = user.getSignedJwtToken();
+    res.status(200).json({ success: true, token });
+  } catch (err) {
+    next(err);
+  }
+};
