6a90e6bf57fbef17233dbd689c789a7fc02290ea
ashish_kadam <ashish.kadam@quantiphi.com>
2023-07-24 00:53:46 +0530

    Bootcamp & User Relationship:A publisher can add only one bootcamp.Admin can add any number of bootcamps.

--- seeder.js
+++ seeder.js
@@ -7,8 +7,9 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config/config.env" });
 
 // Load models
-const Bootcamp = require("./models/bootcamp");
+const Bootcamp = require("./models/Bootcamp");
 const Course = require("./models/Course");
+const User = require("./models/User");
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
@@ -24,11 +25,15 @@ const bootcamps = JSON.parse(
 const courses = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
 );
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, "utf-8")
+);
 // Import into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
+    await User.create(users);
     console.log("Data Imported...".green.inverse);
     process.exit();
   } catch (err) {
@@ -41,6 +46,7 @@ const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
+    await User.deleteMany();
     console.log("Data Destroyed...".red.inverse);
     process.exit();
   } catch (err) {
