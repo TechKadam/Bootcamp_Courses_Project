0731c9896e5a6a5ff49fd1fb1162c17b1f95b214
ashish_kadam <ashish.kadam@quantiphi.com>
2023-07-25 18:20:54 +0530

    Forgot Password:Send Email

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const ErrorResponse = require("../utils/errorResponse");
 const User = require("../models/User");
+const sendEmail = require("../utils/sendEmail");
 
 // @desc     Register a user
 // @route    GET /api/v1/auth/register
@@ -98,7 +99,26 @@ exports.forgotPassword = async (req, res, next) => {
     // Get reset token
     const resetToken = user.getResetPasswordToken();
     await user.save({ validateBeforeSave: false });
-    console.log(resetToken);
+    // Create reset URL
+    const resetURL = `${req.protocol}://${req.get(
+      "host"
+    )}/api/v1/resetpassword/${resetToken}`;
+    const message = `You are receiving this email because you (or someone else) has requested the reset of a password. Please make a PUT request to:\n\n${resetURL}`;
+    try {
+      await sendEmail({
+        email: user.email,
+        subject: "Password reset token",
+        message,
+      });
+      res.status(200).json({ success: true, data: "Email sent" });
+    } catch (err) {
+      console.log(err);
+      user.resetPasswordToken = undefined;
+      user.resetPasswordExpire = undefined;
+
+      await user.save({ validateBeforeSave: false });
+      return next(new ErrorResponse("Email could not be sent", 500));
+    }
     res.status(200).json({ success: true, data: user });
   } catch (err) {
     next(err);
